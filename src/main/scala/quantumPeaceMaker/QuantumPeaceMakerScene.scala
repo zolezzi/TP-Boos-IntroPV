5efe8bbcc32b0eda02c5e93baa7e81c9a777f56f
zolezzi <carlos_cardozo@live.com>
2016-09-03 00:22:37 -0300

    Creacion de clase y movimiento horizontal de QuantumShip

---
+++ src/main/scala/quantumPeaceMaker/QuantumPeaceMakerScene.scala
@@ -0,0 +1,8 @@
+package quantumPeaceMaker
+
+import com.uqbar.vainilla.GameScene
+
+class QuantumPeaceMakerScene extends GameScene{
+  this.addComponent(EnemySpawner)
+  this.addComponent(QuantumShip)
+}
